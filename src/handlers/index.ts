90153457df0d74b77031e91b4ac19c6f43807739
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-04-22 17:15:29 -0600

    Agregando types al req y res mi funcion para crear usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,8 @@
+import type { Request, Response } from 'express';
+
 import User from "../models/User";
 
-export const createAccount = async (req, res) => {
+export const createAccount = async (req: Request, res: Response) => {
 
     const user = new User(req.body);
 
