bff90e8355fbb5287d1d5a51b7445fdd4382b98a
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-04-22 17:10:19 -0600

    Creando funcion independiente para registrar usuarios

---
+++ src/handlers/index.ts
@@ -0,0 +1,10 @@
+import User from "../models/User";
+
+export const createAccount = async (req, res) => {
+
+    const user = new User(req.body);
+
+    await user.save();
+
+    res.send('Usuario registrado correctamente');
+}
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
-import User from './models/User';
+import { createAccount } from './handlers';
 
 const router = Router();
 
 //* Auth and register routes
-router.post('/auth/register', async (req, res) => {
-
-    await User.create(req.body);
-})
+router.post('/auth/register', createAccount)
 
 
 
