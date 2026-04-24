5e6ea1427bb33b42abce6968b0bacdb6a3fe2260
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-04-24 16:28:39 -0600

    Se agrega nombre de usuario para las cuentas

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import slug from 'slug';
 
 import User from "../models/User";
 import { hashPassword } from '../utils/auth';
@@ -10,13 +11,22 @@ export const createAccount = async (req: Request, res: Response) => {
     const userExists = await User.findOne({ email })
 
     if (userExists) {
-        const error = new Error('El usuario ya está registrado');
+        const error = new Error('Un usuario con ese email ya existe');
+        return res.status(409).json({ error: error.message });
+    }
+
+    const handle = slug(req.body.handle, '');
+    const handleExists = await User.findOne({ handle })
+    if (handleExists) {
+        const error = new Error('Nombre de usuario no disponible');
         return res.status(409).json({ error: error.message });
     }
 
     const user = new User(req.body);
 
     user.password = await hashPassword(password);
+
+    user.handle = handle;
 
     await user.save();
 
