53c8b6ff9d04b5172be933029d492ea633d0c5b2
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-04-24 16:11:31 -0600

    Hasheando password del usuario

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,12 +1,24 @@
 import type { Request, Response } from 'express';
 
 import User from "../models/User";
+import { hashPassword } from '../utils/auth';
 
 export const createAccount = async (req: Request, res: Response) => {
 
+    const { email, password } = req.body;
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        const error = new Error('El usuario ya está registrado');
+        return res.status(409).json({ error: error.message });
+    }
+
     const user = new User(req.body);
+
+    user.password = await hashPassword(password);
 
     await user.save();
 
-    res.send('Usuario registrado correctamente');
+    res.status(201).send('Usuario registrado correctamente');
 }
